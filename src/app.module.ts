de506bdf02ebac4d9ef585cf2df69f444728649e
parksy-13 <psy9905@gmail.com>
2024-03-13 21:18:04 +0900

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,61 @@
+import Joi from 'joi';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+import { AuthModule } from './auth/auth.module';
+import { Ticket } from './support-message/entities/support-message.entity';
+import { TicketModule } from './support-message/support-message.module';
+import { Performance } from './team/entities/team.entity';
+import { PerformanceModule } from './team/team.module';
+import { Hall } from './team/entities/team.entity';
+import { HallModule } from './team/team.module';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
+
+const typeOrmModuleOptions = {
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'mysql',
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    database: configService.get('DB_NAME'),
+    entities: [User, Performance, Hall, Ticket],
+    synchronize: configService.get('DB_SYNC'),
+    logging: true,
+  }),
+  inject: [ConfigService],
+};
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET_KEY: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+        DB_SYNC: Joi.boolean().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    AuthModule,
+    UserModule,
+    TicketModule,
+    PerformanceModule,
+    HallModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,36 @@
+import { Ticket } from 'src/ticket/entities/ticket/entity';
+import { Point } from 'src/point/entities/point/entity';
+
+import {
+  Column,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { Role } from '../types/userRole.type';
+
+@Index('email', ['email'], { unique: true })
+@Entity({
+  name: 'users',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  userId: number;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', unique: true, nullable: false })
+  nickname: string;
+
+  @Column({ type: 'varchar', select: false, nullable: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.customer })
+  role: Role;
+
+  @OneToMany(() => Ticket, (ticket) => ticket.user)
+  tickets: Ticket[];
+}
